9f069ea578a3e769d5279aa321dff44593a2e4ac
pedroa912 <palvarezbrescia@udesa.edu.ar>
2023-04-04 18:32:02 -0300

    base de datos actualizada

--- parcial1/controllers/celularesController.js
+++ parcial1/controllers/celularesController.js
@@ -1,11 +1,28 @@
-const data = require('../db/celularesDatos')
+const celulares = require('../db/celularesDatos');
 
 const celularesController = {
-    'index' : function (req,res) {
-        res.render('index')
+    index : function(req, res) {
+        return res.render('products', {
+            lista: celulares.lista,
+            mensaje: "Todos los celulares",
+            x : true
+        });
     },
-    'marcas' : function () {
-        
+    marcas :function(req, res) {
+        let brand = req.params.brand;
+        let resultado = [];
+    
+        for (let i = 0; i < autos.lista.length; i++) {
+            if (brand == autos.lista[i].marca) {
+                resultado.push(autos.lista[i])
+            }
+        }
+    
+        if (resultado.length == 0) {
+            return res.send('No hay datos para la marca ' + brand);
+        } else {
+            return res.send(resultado);
+        }
     },
     'modelos' : function () {
         
--- parcial1/db/celularesDatos.js
+++ parcial1/db/celularesDatos.js
@@ -1,80 +1,145 @@
 const celulares = {
-    iphone : [
+    lista: [
         {
             id : 1,
             modelo : "iphone 14",
             descripcion :  "Pantalla Super Retina XDR, Pantalla OLED de 6.1 pulgadas (diagonal) sin marco y Resolución de 2532 x 1170 pixeles a 460 ppi ",
             foto : 'images/iphone-14.png', 
             fecha : " 25/01/2023",
-
-        },{
+            comentarios: [
+                {
+                    id: 1,
+                    usuario: "juan",
+                    texto:"Muy buenas zapatillas",
+                },
+            ]
+        },
+ 
+        {
             id : 2,
             modelo : "iphone 14 pro",
             descripcion : " Pantalla Super Retina XDR, Pantalla OLED de 6.1 pulgadas (diagonal) sin marco y Resolución de 2556 x 1179 pixeles a 460 ppi",
             foto : 'images/iphone-14-pro.png' ,
             fecha : "17/01/2023",
-            
-        },{
+            comentarios: [
+                {
+                    id: 2,
+                    usuario: "Gianfranco",
+                    texto:"Muy buenas zapatillas",
+                },
+            ]
+        },
+        {
             id : 3,
             modelo : "iphone 14 pro max",
             descripcion : "Pantalla Super Retina XDR, Pantalla OLED de 6.7 pulgadas (diagonal) sin marco y Resolución de 2796 x 1290 pixeles a 460 ppi",
             foto : "images/iphone-14-pro-max.webp ",
             fecha : "03/02/2023 ",
-            
-        },{
+            comentarios: [
+                {
+                    id: 3,
+                    usuario: "Bautista",
+                    texto:"Muy buenas zapatillas",
+                },
+            ]
+        },
+        {
             id : 4,
             modelo : "iphone 13",
             descripcion : "Pantalla Super Retina XDR, Pantalla OLED de 6.1 pulgadas (diagonal) sin marco y Resolución de 2532 x 1170 pixeles a 460 ppi",
             foto : "images/iphone-13.png",
             fecha : "13/09/2021 ",
-            
-        },{
+            comentarios: [
+                {
+                    id: 4,
+                    usuario: "Olivier",
+                    texto:"La calidad no era lo que esperaba",
+                },
+            ]
+        },
+        {
             id : 5,
             modelo : "iphone SE(tercera generacion)",
             descripcion : "Pantalla Retina HD, Pantalla widescreen LCD Multi-Touch de 4.7 pulgadas (diagonal) con tecnología IPS y Resolución de 1334 x 750 pixeles a 326 ppi",
             foto : "images/iphone-se-3ra.jpg",
             fecha : "13/09/2020 ",
-            
-        }
-    ],
-    samsung : [
+            comentarios: [
+                {
+                    id: 5,
+                    usuario: "Matias",
+                    texto:"Muy facheras pero me hicieron doler los pies",
+                },
+            ]
+        },
         {
-        id : 1,
-        modelo : "Galaxy A34",
-        descripcion :  "Con una pantalla Super AMOLED de 6.6 pulgadas a resolución Full HD+ y tasa de refresco de 120Hz",
-        foto :   "/images/samsung-a34.png", 
-        fecha : " 23/01/2023"
-
-    },{
-        id : 2,
-        modelo : "Galaxy S23 ultra",
-        descripcion : "se alimenta de una batería de 5000 mAh con soporte para carga rápida de 45W y soporta carga inalámbrica, completando sus características con un stylus S Pen, parlantes stereo y lector de huellas bajo pantalla",
-        foto : "images/samsung-s23-ultra.jpg",
-        fecha : "04/04/2023"
-        
-    },{
-        id : 3,
-        modelo : "Galaxy A54",
-        descripcion : "pantalla AMOLED de 6.4 pulgadas con resolución FullHD+ y 120 Hz, batería de 5,000 mAh, 6 GB de RAM y 128 GB de almacenamiento, y cámara principal de 50 megapixeles.",
-        foto : "/images/samsung-a54.avif",
-        fecha : "15/03/2023"
-        
-    },{
-        id : 4,
-        modelo : "Galaxy S23+",
-        descripcion : "Cámara principal: sensor de 50 MP, f/1.8, 24mm, 1/1.56, 1.0µm, PDAF, OIS Cámara teleobjetivo: sensor de 10 MP, f/2.4, 70mm teleobjetivo, 1/3.94, 1.0µm, PDAF, OIS,3x Cámara ultra gran angular",
-        foto : "images/samsung-s23-plus.jpg",
-        fecha : "04/04/2023"
-        
-    },{
-        id : 5,
-        modelo : "j5",
-        descripcion : "Tiene una pantalla Super AMOLED PenTile de 5 y un procesador 1.2 GHz de cuatro núcleos Snapdragon 410. ",
-        foto : "images/samsung-j5.jpg",
-        fecha : "10/7/2018"
-
-    }]
-}
+            id: 6,
+            modelo: "iphone 12",
+            descripcion: " Pantalla Super Retina XDR, Pantalla OLED de 6.1 pulgadas (diagonal) sin marco, Resolución de 2532 x 1170 pixeles a 460 ppi",
+            imagen: "jordan1.jpg",
+            comentarios: [
+                {
+                    id: 6,
+                    usuario: "Baltazar",
+                    texto:"La mejor compra en mucho timpo!",
+                },
+            ]
+        },
+        {
+            id: 7,
+            modelo: "iphone 11",
+            descripcion: " Liquid Retina HD, Pantalla LCD Multi-Touch de 6,1 pulgadas (en diagonal) con tecnología IPS, Resolución de 1.792 por 828 píxeles a 326 p/p",
+            imagen: "jordan1.jpg",
+            comentarios: [
+                {
+                    id: 7,
+                    usuario: "Micaela",
+                    texto:"Me gustarian mas con otros colores, son dificiles de convinar",
+                },
+            ]
+        },
+        {
+            id: 8,
+            modelo: "iphone 11 pro",
+            descripcion: "Super Retina XDR, Pantalla OLED Multi-Touch de 5,8 pulgadas (en diagonal), Pantalla HDR, Resolución de 2.436 por 1.125 píxeles a 458 p/p",
+            imagen: "jordan1.jpg",
+            comentarios: [
+                {
+                    id: 8,
+                    usuario: "Nicole",
+                    texto:"Se demoraron con el envio pero cumplieron mis expectativas",
+                },
+            ]
+        },
+        {
+            id: 9,
+            modelo: "iphone x",
+            descripcion: "El iPhone X es un teléfono inteligente de Apple con pantalla OLED de 5.8 pulgadas, cámara trasera doble de 12 megapíxeles, procesador A11 Bionic, opciones de almacenamiento interno de 64 GB o 256 GB, carga inalámbrica y Face ID.",
+            imagen: "jordan1.jpg",
+            comentarios: [
+                {
+                    id: 9,
+                    usuario: "Maria Jose",
+                    texto:"Zapatillas muy buenas para el dia a dia!",
+                },
+            ]
+        },
+        {
+            id: 10,
+            modelo: "iphone 8",
+            descripcion: "El iPhone 8 es un teléfono inteligente de Apple con pantalla LCD de 4.7 pulgadas, cámara trasera de 12 megapíxeles, procesador A11 Bionic, opciones de almacenamiento interno de 64 GB o 256 GB, carga inalámbrica y Touch ID.",
+            imagen: "jordan1.jpg",
+            comentarios: [
+                {
+                    id: 10,
+                    usuario: "Maite",
+                    texto:"Las habia pedido en color rosa y me mandaron en rojo! No responden los mensajes.",
+                },
+            ]
+        },
+ 
+ 
+    ]
+};
 
 
 module.exports = celulares
