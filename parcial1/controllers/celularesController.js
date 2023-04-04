6d2e5fb5ff5c593738eabe1973d273ecc843fd4d
pedroa912 <palvarezbrescia@udesa.edu.ar>
2023-04-04 18:52:24 -0300

    funcion index y busqueda por modelo

--- parcial1/controllers/celularesController.js
+++ parcial1/controllers/celularesController.js
@@ -2,24 +2,24 @@ const celulares = require('../db/celularesDatos');
 
 const celularesController = {
     index : function(req, res) {
-        return res.render('products', {
+        return res.render('celulares', {
             lista: celulares.lista,
             mensaje: "Todos los celulares",
             x : true
         });
     },
-    marcas :function(req, res) {
-        let brand = req.params.brand;
+    modelos :function(req, res) {
+        let modelos = req.params.modelo;
         let resultado = [];
     
-        for (let i = 0; i < autos.lista.length; i++) {
-            if (brand == autos.lista[i].marca) {
-                resultado.push(autos.lista[i])
+        for (let i = 0; i < celulares.lista.length; i++) {
+            if (modelos == celulares.lista[i].modelo) {
+                resultado.push(celulares.lista[i])
             }
         }
     
         if (resultado.length == 0) {
-            return res.send('No hay datos para la marca ' + brand);
+            return res.send('No hay datos para el modelo' + modelos);
         } else {
             return res.send(resultado);
         }
