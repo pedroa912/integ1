941edcefdf947f5d5cb288233b2cbf2181e1e4ff
pedroa912 <palvarezbrescia@udesa.edu.ar>
2023-04-05 13:11:32 -0300

    controlador

--- parcial1/controllers/celularesController.js
+++ parcial1/controllers/celularesController.js
@@ -1,41 +1,37 @@
-const celulares = require('../db/celularesDatos');
+const data = require('../db/celularesDatos');
 
 const celularesController = {
-    index : function(req, res) {
-        return res.render('celulares', {
-            lista: celulares.lista,
-            mensaje: "Todos los celulares",
-            x : true
-        });
+    // index : function(req, res) {
+    //     return res.render('celulares', {
+    //         lista: celulares.lista,
+    //         mensaje: "Todos los celulares",
+    //         x : true
+    //     });
+    // },
+    index : function (req, res) {
+        return res.render('index', {celulares: data.lista})
     },
-    modelos :function(req, res) {
-        let modelos = req.params.modelo;
-        let resultado = [];
+    // modelos :function(req, res) {
+    //     let modelos = req.params.modelo;
+    //     let resultado = [];
     
-        for (let i = 0; i < celulares.lista.length; i++) {
-            if (modelos == celulares.lista[i].modelo) {
-                resultado.push(celulares.lista[i])
-            }
-        }
+    //     for (let i = 0; i < celulares.lista.length; i++) {
+    //         if (modelos == celulares.lista[i].modelo) {
+    //             resultado.push(celulares.lista[i])
+    //         }
+    //     }
     
-        if (resultado.length == 0) {
-            return res.send('No hay datos para el modelo' + modelos);
-        } else {
-            return res.send(resultado);
-        }
+    //     if (resultado.length == 0) {
+    //         return res.send('No hay datos para el modelo' + modelos);
+    //     } else {
+    //         return res.send(resultado);
+    //     }
+    // },
+    add: function (req, res) {
+        return res.render('product-add')
     },
-    'modelos' : function () {
-        
-    },
-   'comentarios' : function () {
-        
+    show: function (req, res) {
+        return res.render('product', {celular: data.lista})
     }
-
-
-
-
-
-
 }
-
 module.exports = celularesController
