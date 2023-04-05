976d15656be51fd89d09cdd274d3455768aeb222
lolo-anino <lanino@udesa.edu.ar>
2023-04-05 18:45:40 -0300

    include

--- parcial1/app.js
+++ parcial1/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var celularesRouter = require('./routes/celulares');
+var addRouter = require('./routes/celulares');
 
 
 var app = express();
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/celulares', celularesRouter);
+app.use('/add', addRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- parcial1/routes/celulares.js
+++ parcial1/routes/celulares.js
@@ -4,9 +4,10 @@ const productController = require('../controllers/celularesController');
 
 
 router.get('/', productController.index);
+
 router.get('/add', productController.add)
 
-router.get('/celulares', productController.modelos);
+router.get('/celulares', productController.show);
 
 //router.get('/productos/modelo/:id', productController.index);
 
