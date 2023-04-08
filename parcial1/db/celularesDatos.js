e437c7581e5585513c40dbfa041baf1c71cf86dd
pedroa912 <palvarezbrescia@udesa.edu.ar>
2023-04-08 12:04:03 -0300

    index ejs

--- parcial1/db/celularesDatos.js
+++ parcial1/db/celularesDatos.js
@@ -4,7 +4,7 @@ const celulares = {
             id : 1,
             modelo : "iphone 14",
             descripcion :  "Pantalla Super Retina XDR, Pantalla OLED de 6.1 pulgadas (diagonal) sin marco y Resolución de 2532 x 1170 pixeles a 460 ppi ",
-            foto : 'images/iphone-14.png', 
+            foto : '/images/products/iphone-14.png', 
             fecha : " 25/01/2023",
             comentarios: [
                 {
@@ -19,7 +19,7 @@ const celulares = {
             id : 2,
             modelo : "iphone 14 pro",
             descripcion : " Pantalla Super Retina XDR, Pantalla OLED de 6.1 pulgadas (diagonal) sin marco y Resolución de 2556 x 1179 pixeles a 460 ppi",
-            foto : 'images/iphone-14-pro.png',
+            foto : '/images/products/iphone-14-pro.png',
             fecha : "17/01/2023",
             comentarios: [
                 {
@@ -33,7 +33,7 @@ const celulares = {
             id : 3,
             modelo : "iphone 14 pro max",
             descripcion : "Pantalla Super Retina XDR, Pantalla OLED de 6.7 pulgadas (diagonal) sin marco y Resolución de 2796 x 1290 pixeles a 460 ppi",
-            foto : "images/iphone-14-pro-max.webp ",
+            foto : "/images/products/iphone-14-pro-max.webp ",
             fecha : "03/02/2023",
             comentarios: [
                 {
@@ -47,7 +47,7 @@ const celulares = {
             id : 4,
             modelo : "iphone 13",
             descripcion : "Pantalla Super Retina XDR, Pantalla OLED de 6.1 pulgadas (diagonal) sin marco y Resolución de 2532 x 1170 pixeles a 460 ppi",
-            foto : "images/iphone-13.png",
+            foto : "/images/products/iphone-13.png",
             fecha : "13/09/2021",
             comentarios: [
                 {
@@ -61,7 +61,7 @@ const celulares = {
             id : 5,
             modelo : "iphone SE(tercera generacion)",
             descripcion : "Pantalla Retina HD, Pantalla widescreen LCD Multi-Touch de 4.7 pulgadas (diagonal) con tecnología IPS y Resolución de 1334 x 750 pixeles a 326 ppi",
-            foto : "images/iphone-se-3ra.jpg",
+            foto : "/images/products/iphone-se-3ra.jpg",
             fecha : "13/09/2020 ",
             comentarios: [
                 {
@@ -75,7 +75,7 @@ const celulares = {
             id: 6,
             modelo: "iphone 12",
             descripcion: " Pantalla Super Retina XDR, Pantalla OLED de 6.1 pulgadas (diagonal) sin marco, Resolución de 2532 x 1170 pixeles a 460 ppi",
-            imagen: "images/iphone-12.jpg",
+            imagen: "/images/products/iphone-12.jpg",
             fecha : "03/01/2023",
             comentarios: [
                 {
@@ -89,7 +89,7 @@ const celulares = {
             id: 7,
             modelo: "iphone 11",
             descripcion: " Liquid Retina HD, Pantalla LCD Multi-Touch de 6,1 pulgadas (en diagonal) con tecnología IPS, Resolución de 1.792 por 828 píxeles a 326 p/p",
-            imagen: "images/iphone-11.png",
+            imagen: "/images/products/iphone-11.png",
             fecha : "22/08/2022",
             comentarios: [
                 {
@@ -103,7 +103,7 @@ const celulares = {
             id: 8,
             modelo: "iphone 11 pro",
             descripcion: "Super Retina XDR, Pantalla OLED Multi-Touch de 5,8 pulgadas (en diagonal), Pantalla HDR, Resolución de 2.436 por 1.125 píxeles a 458 p/p",
-            imagen: "images/iphone-11-pro.png",
+            imagen: "/images/products/iphone-11-pro.png",
             fecha : "24/10/2022",
             comentarios: [
                 {
@@ -117,7 +117,7 @@ const celulares = {
             id: 9,
             modelo: "iphone x",
             descripcion: "El iPhone X es un teléfono inteligente de Apple con pantalla OLED de 5.8 pulgadas, cámara trasera doble de 12 megapíxeles, procesador A11 Bionic, almacenamiento interno de 256 GB, carga inalámbrica y Face ID.",
-            imagen: "images/iphone-x.png",
+            imagen: "/images/products/iphone-x.png",
             fecha : "28/09/2021",
             comentarios: [
                 {
@@ -131,7 +131,7 @@ const celulares = {
             id: 10,
             modelo: "iphone 8",
             descripcion: "El iPhone 8 es un teléfono inteligente de Apple con pantalla LCD de 4.7 pulgadas, cámara trasera de 12 megapíxeles, procesador A11 Bionic, almacenamiento interno de 64 GB, carga inalámbrica y Touch ID.",
-            imagen: "images/iphone-8.png",
+            imagen: "/images/products/iphone-8.png",
             fecha : "06/04/2020",
             comentarios: [
                 {
